9df02458f72cf551cd76459fbb40b01d9f56e276
Augusto Pascutti <augusto@phpsp.org.br>
2012-05-12 03:15:24 -0300

    Covergae improvement

--- tests/Aeronautics/Spitfire/ApplicationTest.php
+++ tests/Aeronautics/Spitfire/ApplicationTest.php
@@ -21,4 +21,11 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
         $response = $this->app->dispatch('GET', '/');
         $this->assertEquals('Crimsom Rider...', (string) $response);
     }
+
+    public function testToString()
+    {
+        $_SERVER['REQUEST_URI']    = '/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->assertEquals('Crimsom Rider...', (string) $this->app);
+    }
 }
