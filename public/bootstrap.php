44b4cc31200d22d53a21f0ba81d80b67768d5d4a
Augusto Pascutti <augusto@phpsp.org.br>
2012-05-12 12:43:07 -0300

    Added first parser
    * Parses presences in given sessions by date
    * Added in README organization a suggested structure for URI

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set('UTC');
+date_default_timezone_set('America/Sao_Paulo');
 ini_set('display_errors',1);
 error_reporting(-1);
 
@@ -7,3 +7,4 @@ chdir(__DIR__.'/../');
 set_include_path('src'.PATH_SEPARATOR.get_include_path());
 
 spl_autoload_register(require 'Respect/Loader.php');
+define('TEST_DATA', realpath('tests/data'));
