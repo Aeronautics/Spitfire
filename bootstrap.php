37e5a583fc98cf977c3cd3a5d370d77ae3d7c795
Henrique Moody <henriquemoody@gmail.com>
2012-05-24 15:03:27 -0300

    Adicionada configuração para o composer.

--- bootstrap.php
+++ bootstrap.php
@@ -3,25 +3,28 @@
 /* Timezone */
 date_default_timezone_set('America/Sao_Paulo');
 
+/* Autoloader */
+if (!($autoload = @include __DIR__ . '/vendor/autoload.php')) {
 
-/* Include path */
-chdir(__DIR__);
-set_include_path(implode(PATH_SEPARATOR, array(
-    __DIR__ . '/src',
-    get_include_path(),
-)));
+    /* Include path */
+    set_include_path(implode(PATH_SEPARATOR, array(
+        __DIR__ . '/src',
+        get_include_path(),
+    )));
 
-/* PEAR autoloader */
-spl_autoload_register(
-    function($className) {
-        $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
-        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-            $path .= DIRECTORY_SEPARATOR . $filename;
-            if (is_file($path)) {
-                require_once $path;
-                return true;
+    /* PEAR autoloader */
+    spl_autoload_register(
+        function($className) {
+            $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
+            foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+                $path .= DIRECTORY_SEPARATOR . $filename;
+                if (is_file($path)) {
+                    require_once $path;
+                    return true;
+                }
             }
+            return false;
         }
-        return false;
-    }
-);
+    );
+
+}
